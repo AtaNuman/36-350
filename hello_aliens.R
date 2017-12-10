cabbb9ce55dd868dbafbf126023ae26947db20dd
Ata Numan <ata@numan.us>
2017-12-10 13:30:49 -0500

    yes

---
+++ hello_aliens.R
@@ -0,0 +1,23 @@
+cat("To whom it may concern,
+    My name is Ata Numanbayraktaroglu, and I am from the planet Earth, 
+    located in the Milky-way galaxy, and the third planet away from the
+    Sun. We mean no harm. Our planet is the only known planet to hold 
+    any sign of life in the observable spectrum from our perspective so 
+    we are unable to determine if this message will reach anyone. We
+    consider ourselves intelligent to the point that we are able to
+    venture off to other planets using drones and gather information on 
+    them. I am using a language named English, which is a very popular
+    language in our planet. My apologies if you do not understand. Our 
+    world is split into countries, each with their government and 
+    culture. Much like the dream many people around Earth have of 
+    travelling to many countries, we wish to travel to other planets and
+    possibly galaxies in the future. We do not seek to invade nor 
+    terrorize other planets, but simply visit and gather information. 
+    If you wish to make further contact, which we hope you do, please 
+    send a radio wave back to us and we will establish a more efficient 
+    means of communications from there. 
+    
+    Thank you very much, 
+    Best regards, 	
+    Ata Numanbayraktaroglu"
+)
